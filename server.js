b908afdb9edd54f3f8af26a025a72364a4125bb5
Jason Santos <jslum18@hotmail.com>
2022-03-01 16:29:46 -0500

    npm install mongoose

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const nftsController = require('./controllers/nfts.js');
 app.use('/nfts', nftsController);
 
 app.listen(3000, ()=>{
     console.log('listening...');
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/merncrud')
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
 });
